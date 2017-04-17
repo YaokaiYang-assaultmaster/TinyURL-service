266ddf3bfb5f412a904e3501a3aad2ccf7da0208
Yaokai Yang <yxy586@case.edu>
2017-04-16 21:41:08 -0400

    Update server.js

--- app/server.js
+++ app/server.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
 var useragent = require('express-useragent');
 
-mongoose.connect("mongodb://user:tinyurl@ds053126.mlab.com:53126/tinyurldb");
+mongoose.connect("[put your mongodb URL here]");
 
 app.use("/public", express.static(__dirname + "/public"));
 app.use("/node_modules", express.static(__dirname + "/node_modules"));
